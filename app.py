d3fc3e363764363037c295ffd9658807e9e7c9f5
ajmunerau <60147060+ajmunerau@users.noreply.github.com>
2023-08-24 07:42:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -30,3 +30,24 @@ with col2:
        st.write('La audición es fundamental para tu interfaz')
     if modo == 'Táctil':
        st.write('El tacto es fundamental para tu interfaz')
+
+st.subheader("Selectbox")
+in_mod = st.selectbox(
+    "Selecciona la modalidad",
+    ("Audio", "Visual", "Háptico"),
+)
+if in_mod == "Audio":
+    set_mod = "Reproducir audio"
+elif in_mod == "Visual":
+    set_mod = "Reproducir video"
+elif in_mod == "Háptico":
+    set_mod = "Activar vibración"
+st.write(" La acción es:" , set_mod)
+
+
+with st.sidebar:
+    st.subheader("Configura la modalidad")
+    mod_radio = st.radio(
+        "Escoge la modalidad a usar",
+        ("Visual", "Auditiva","Háptica")
+    )
