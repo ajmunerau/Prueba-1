e836ed3e5ad17a3cba3fe951d07cce390b47cfb0
ajmunerau <60147060+ajmunerau@users.noreply.github.com>
2023-08-24 07:44:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import streamlit as st
-from PIL import Image 
+from PIL import Image
 
-st.write("Mi primera aplicación")
+st.title(" Mi Primera App!!")
 
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open('TVU VS. CLUBMERC.jpg')
 
-st.image(image, caption = 'Hola Soy Lasso')
+st.image(image, caption='Interfaces multimodales')
 
-texto = st.text_input('Escribe algo','Este es mi texto')
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
 st.write('El texto escrito es', texto)
 
 st.subheader("Ahora usemos 2 Columnas")
@@ -30,6 +33,12 @@ with col2:
        st.write('La audición es fundamental para tu interfaz')
     if modo == 'Táctil':
        st.write('El tacto es fundamental para tu interfaz')
+        
+st.subheader("Uso de Botones")
+if st.button('Presiona el botón'):
+    st.write('Gracias por presionar')
+else:
+    st.write('No has presionado aún')
 
 st.subheader("Selectbox")
 in_mod = st.selectbox(
