8ac26a7eccf96d96ca0862449ae051c39ee722d5
ajmunerau <60147060+ajmunerau@users.noreply.github.com>
2023-08-24 07:18:31 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,7 @@ st.write("Mi primera aplicación")
 
 image = Image.open('TVU VS. CLUBMERC.jpg')
 
-
 st.image(image, caption = 'Hola Soy Lasso')
+
+texto = st.text_input('Escribe algo','Este es mi texto')
+st.write('El texto escrito es', texto)
